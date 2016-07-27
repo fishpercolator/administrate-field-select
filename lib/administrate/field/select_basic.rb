682bc29f084b027b0da06dd283417541dadd2d93
Rahoul Baruah <rahoul@3hv.co.uk>
2016-07-27 09:41:27 +0100

    Refactored to remove named parameters

--- lib/administrate/field/select_basic.rb
+++ lib/administrate/field/select_basic.rb
@@ -5,29 +5,37 @@ module Administrate
   module Field
     class SelectBasic < Base
       class Engine < ::Rails::Engine
-      end      
-      
+      end
+
       def choices
-        options.fetch(:choices, []).map { |o| convert(o) }
+        options.fetch(:choices, []).map { |o| convert_to_array(o) }
       end
-  
+
       def to_s
-        convert(data, raw: true)
+        convert(data)
       end
-  
+
       def include_blank
         options.fetch(:include_blank, false)
       end
-    
+
       private
 
-      def convert item, raw: false
-        return [prettify(item), item] if prettify? && !raw
-        return prettify(item) if prettify? && raw
-        return internationalise(item, raw: raw) if i18n?
+      def convert_to_array item
+        return item if do_not_modify_contents?
+        item.respond_to?(:each) ? [convert(item.first), item.last] : [convert(item), item]
+      end
+
+      def convert item
+        return prettify(item) if prettify?
+        return internationalise(item) if i18n?
         return item
       end
-  
+
+      def do_not_modify_contents?
+        !(prettify? || i18n?)
+      end
+
       def prettify?
         !!options[:prettify]
       end
@@ -35,18 +43,17 @@ module Administrate
       def i18n?
         !!options[:i18n]
       end
-  
-      def prettify(str)
-        if options[:prettify].respond_to? :call
-          options[:prettify].call(str)
-        else
-          str.titleize
-        end
+
+      def callable_prettify?
+        options[:prettify].respond_to? :call
       end
 
-      def internationalise item, raw: false
-        return I18n.t(item) if raw
-        item.respond_to?(:each) ? [I18n.t(item.first), item.last] : [I18n.t(item), item]
+      def prettify(str)
+        callable_prettify? ? options[:prettify].call(str) : str.titleize
+      end
+
+      def internationalise item
+        return I18n.t(item)
       end
     end
   end
